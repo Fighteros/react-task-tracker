e3895a538b10131b6d116e9c72b8d4f707c30ee3
fighteros <fighteros121212@gmail.com>
2021-09-01 08:13:25 +0200

    added tasks useState to app level

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import Header from './components/Header'
 import Task from './components/Task'
+import { useState } from 'react';
+
 // import React from 'react'
 
 // lmbda 
 const App = () => {
+  // we wont use .push as state is immutable 
+  // we instead use setTask and recreate it
+  const [tasks, setTasks] = useState(
+    [
+      {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "Feb 5th at 2:30pm",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Meeting at School",
+        day: "Feb 6th at 1:30pm",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "Food Shopping",
+        day: "Feb 5th at 2:30pm",
+        reminder: false,
+      },
+    ]
+  )
   return (
     <div className="container">
       <Header />
-      <Task />
+      <Task tasks={tasks} />
     </div>
   );
 }
