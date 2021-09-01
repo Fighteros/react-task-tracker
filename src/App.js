0eb53ed59824c7964c5d183637eca88d556da369
fighteros <fighteros121212@gmail.com>
2021-09-01 15:30:38 +0200

    add task through server request

--- src/App.js
+++ src/App.js
@@ -51,14 +51,27 @@ const App = () => {
     )
   }
 
-  // add task
-  const addTask = (task) => {
-    // console.log(task)
-    // to get unique random number
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'Application/json'
+      },
+      body: JSON.stringify(task)
+    })
+    const data = await res.json()
+    setTasks([...tasks, data])
   }
+
+
+  // add task locally it's not a good example anyways
+  // const addTask = (task) => {
+  //   // console.log(task)
+  //   // to get unique random number
+  //   const id = Math.floor(Math.random() * 10000) + 1
+  //   const newTask = { id, ...task }
+  //   setTasks([...tasks, newTask])
+  // }
 
   // show or hide add Task
   const toggleAddTask = () => {
