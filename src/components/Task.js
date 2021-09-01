53a8a0a9adcb9b5eaa02d35215acf1f726bbbc62
fighteros <fighteros121212@gmail.com>
2021-09-01 10:32:47 +0200

    added reminder ui

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,7 @@ import { FaTimes } from 'react-icons/fa'
 
 const Task = ({ task, onDelete, onToggle }) => {
     return (
-        <div className='task' onDoubleClick={() => onToggle(task.id)}>
+        <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
             <h3 key={task.id}>{task.text}<FaTimes onClick={() => onDelete(task.id)} style={{ color: 'red' }} /></h3>
             <p>{task.day}</p>
         </div>
